3166c7912c67f5a357c1cec70de39811bf01b65d
Amy Fogelman <abfnj1@gmail.com>
2019-08-23 13:50:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,13 @@ def select_name_and_series_subgenres_of_authors
 end	
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
-end
-
+  "SELECT Series.title
+  FROM characters
+  INNER JOIN series
+  ON characters.series_id = series.id
+  GROUP BY Series.title, species
+  HAVING characters.species = 'human'
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
+end	
 
